f8fccb3554905a6805cf6746fa8c854baab9fe57
Alvaro Santiago Medina <alvaro.medina@mi.unc.edu.ar>
2024-12-04 20:31:53 -0300

    Configuracion para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     base: '/Portfolio-with-React/',
+    build: {
+        target: 'esnext',
+    },
 })
 
