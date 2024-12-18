635879ea82b266c884649aff9151fae1512cb3ad
alvaro-mediina <alvaro.medina@mi.unc.edu.ar>
2024-12-18 13:35:27 -0300

    Logos, flechitas y su transición

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,17 +1,47 @@
 import './App.css'
 import { WelcomeProfileProvider } from '../context/WelcomeProfileProvider';
 import WelcomeProfile from './components/Welcome-Profile/WelcomeProfile'; 
-import React from 'react';
+import arrowL from '../assets/imgs/arrow.svg'
+import arrowR from '../assets/imgs/arrow.svg'
+import React, { useEffect, useState } from 'react';
 
 
 
 function App() {
+    const [isWelcomeCard, setIsWelcomeCard] = useState(false);
     
+    const leftSection = () => {
+        console.log("Click Efectuado")
+    };
+
+    const rightSection = () => {
+        console.log("Click Efectuado")
+    };
+
+    useEffect(()=>{
+        const animationTimer = setTimeout(() => {
+            setIsWelcomeCard(true);
+        }, 4000);
+
+        return () => clearTimeout(animationTimer);
+    },[]);
+
     return (
         <div className="App">
+            {isWelcomeCard && 
+                <div className="arrow-left" onClick={leftSection}>
+                    <img src={arrowR}  draggable="false"/>
+                </div>
+            }
             <WelcomeProfileProvider>
                 <WelcomeProfile/>
             </WelcomeProfileProvider>
+
+            {isWelcomeCard &&
+                <div className="arrow-right" onClick={rightSection}>
+                    <img src={arrowL}  draggable="false"/>
+                </div>
+            }
         </div>
     );
 }
